de410d3075d79f2884592045dfd5eafa88fcb177
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2026-01-06 20:34:38 -0300

    criando e utilizando uma função do mesmo pacote

--- router/router.go
+++ router/router.go
@@ -7,6 +7,8 @@ import (
 )
 
 func Initiliaze() {
+	//Função que está definida no meu arquivo routes.go no pacote router
+	printMe()
 	router := gin.Default()
 
 	router.GET("/ping", func(c *gin.Context) {
---
+++ router/routes.go
@@ -0,0 +1,5 @@
+package router
+
+func printMe() {
+	print("Me!")
+}
